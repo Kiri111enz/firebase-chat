607c0f6b05369314d60480ce04a49516fffbff5e
Kiri111enz <omelyanenkokirill@gmail.com>
2023-05-31 15:06:02 +0300

    popup add vertical padding

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -8,7 +8,7 @@ interface PopupProps {
 
 const Popup: React.FC<PropsWithChildren<PopupProps>> = (props) => (
     <div className="z-10 h-screen bg-black/60">
-        <div className="z-20 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white px-4 rounded-2xl">
+        <div className="z-20 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white px-4 py-2 rounded-2xl">
             {props.showCloseButton !== false && <CloseButton onClick={props.onClose} />}
             {props.children}
         </div>
